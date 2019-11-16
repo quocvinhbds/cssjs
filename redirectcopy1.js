320a7b08e2165026a1825f0952a7ab2c8e9aca63
quocvinhbds <quocvinh.bds@gmail.com>
2019-11-16 20:41:31 +0700

    Update redirectcopy1.js

--- redirectcopy1.js
+++ redirectcopy1.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://sanhungthinhland.com/'>http://faceseo.vn"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://sanhungthinhland.com/du-an-q7-boulevard-nguyen-luong-bang/'>https://sanhungthinhland.com/du-an-q7-boulevard-nguyen-luong-bang/</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
